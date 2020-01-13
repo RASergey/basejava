884d5f5535f11592a570ca970b41bb02eff986a0
RASergey <ReshetkoSergey@mail.ru>
2020-01-13 15:43:33 +0300

    Corrective packages HW

---
+++ src/com/basejava/lesson1/Resume.java
@@ -0,0 +1,15 @@
+package com.basejava.lesson1;
+
+/**
+ * Initial resume class
+ */
+public class Resume {
+
+    // Unique identifier
+    String uuid;
+
+    @Override
+    public String toString() {
+        return uuid;
+    }
+}

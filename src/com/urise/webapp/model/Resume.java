a13dd1dd61da129bb38a0fa70039d347d36004bd
RASergey <ReshetkoSergey@mail.ru>
2020-03-03 16:47:39 +0300

    HW - 6 added a second field Resume class.

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -9,6 +9,7 @@ public class Resume {
 
     // Unique identifier
     private String uuid;
+    private String fullName;
 
     public Resume() {
         this(UUID.randomUUID().toString());
@@ -18,24 +19,18 @@ public class Resume {
         this.uuid = uuid;
     }
 
+    public Resume(String uuid, String fullName) {
+        this.uuid = uuid;
+        this.fullName = fullName;
+    }
+
     public String getUuid() {
         return uuid;
     }
 
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//
-//        Resume resume = (Resume) o;
-//
-//        return uuid.equals(resume.uuid);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return uuid.hashCode();
-//    }
+    public String getFullName() {
+        return fullName;
+    }
 
     @Override
     public String toString() {

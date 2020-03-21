0215537f4bc6be3b84f277c507f1f1b6fbab9857
RASergey <ReshetkoSergey@mail.ru>
2020-03-21 18:40:26 +0300

    HW - 8 create OrganizationList.

--- src/com/urise/webapp/model/section/Organization.java
+++ src/com/urise/webapp/model/section/Organization.java
@@ -4,24 +4,48 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 public class Organization {
-    private final String nameOrganization;
-    private final LocalDate workedDate;
-    private final LocalDate endWorkedDate;
-    private final String position;
-    private final String duty;
-    private final String urlOrganization;
+    private Link homePage;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String position;
+    private String duty;
 
-    public Organization(String nameOrganization, LocalDate workedDate, LocalDate endWorkedDate, String position, String duty, String urlOrganization) {
-        Objects.requireNonNull(nameOrganization, "nameOrganization must not be null");
-        Objects.requireNonNull(workedDate, "workedDate must not be null");
-        Objects.requireNonNull(endWorkedDate, "endWorkedDate must not be null");
-        Objects.requireNonNull(position, "position must not be null");
-        this.nameOrganization = nameOrganization;
-        this.workedDate = workedDate;
-        this.endWorkedDate = endWorkedDate;
+    public Organization(LocalDate startDate, LocalDate endDate, String position, String duty) {
+        this.startDate = startDate;
+        this.endDate = endDate;
         this.position = position;
         this.duty = duty;
-        this.urlOrganization = urlOrganization;
+    }
+
+    public Organization(String nameOrganization, LocalDate startDate, LocalDate endDate, String position, String duty, String urlOrganization) {
+        Objects.requireNonNull(startDate, "workedDate must not be null");
+        Objects.requireNonNull(endDate, "endWorkedDate must not be null");
+        Objects.requireNonNull(position, "position must not be null");
+        this.homePage = new Link(nameOrganization, urlOrganization);
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.position = position;
+        this.duty = duty;
+    }
+
+    public Link getHomePage() {
+        return homePage;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getDuty() {
+        return duty;
     }
 
     @Override
@@ -31,27 +55,28 @@ public class Organization {
 
         Organization that = (Organization) o;
 
-        if (!nameOrganization.equals(that.nameOrganization)) return false;
-        if (!workedDate.equals(that.workedDate)) return false;
-        if (!endWorkedDate.equals(that.endWorkedDate)) return false;
+        if (!homePage.equals(that.homePage)) return false;
+        if (!startDate.equals(that.startDate)) return false;
+        if (!endDate.equals(that.endDate)) return false;
         if (!position.equals(that.position)) return false;
-        if (duty != null ? !Objects.equals(duty, that.duty) : that.duty != null) return false;
-        return urlOrganization != null ? Objects.equals(urlOrganization, that.urlOrganization) : that.urlOrganization == null;
+        return duty != null ? Objects.equals(duty, that.duty) : that.duty == null;
     }
 
     @Override
     public int hashCode() {
-        int result = nameOrganization.hashCode();
-        result = 31 * result + Objects.hash(nameOrganization, workedDate, endWorkedDate, position, duty, urlOrganization);
+        int result = homePage.hashCode();
+        result = 31 * result + startDate.hashCode();
+        result = 31 * result + endDate.hashCode();
+        result = 31 * result + position.hashCode();
+        result = 31 * result + (duty != null ? duty.hashCode() : 0);
         return result;
     }
 
     @Override
     public String toString() {
-        return "\n" + nameOrganization +
-                "\n" + workedDate.getMonthValue() + "/" + workedDate.getYear() + " - " + endWorkedDate.getMonthValue() + "/" + endWorkedDate.getYear() +
+        return "\n" + homePage +
+                "\n" + startDate.getMonthValue() + "/" + startDate.getYear() + " - " + endDate.getMonthValue() + "/" + endDate.getYear() +
                 "\n" + position +
-                "\n" + duty +
-                "\n" + urlOrganization + "";
+                "\n" + duty;
     }
 }

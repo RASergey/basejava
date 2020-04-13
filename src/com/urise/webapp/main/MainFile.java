d81357446f5ba689635fc8c25173ea0705c377f7
RASergey <ReshetkoSergey@mail.ru>
2020-04-13 18:11:24 +0300

    HW - 9 recursive output of directories and files with margins

--- src/com/urise/webapp/main/MainFile.java
+++ src/com/urise/webapp/main/MainFile.java
@@ -1,41 +1,43 @@
 package com.urise.webapp.main;
 
-import java.io.File;
+import com.urise.webapp.exception.StorageException;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class MainFile {
 
     public static void main(String[] args) {
-//        String filePath = "./.gitignore";
-//
-//        File file = new File(filePath);
-//        try {
-//            System.out.println(file.getCanonicalPath());
-//        } catch (IOException e) {
-//            throw new RuntimeException("Error", e);
-//        }
-//
-//        try(FileInputStream fis = new FileInputStream(filePath);) {
-//            System.out.println(fis.read());
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
 
-        String projectPath = "./src";
-        showProjectFile(projectPath);
-    }
+        Path path = Paths.get("./src");
+        try {
+            Files.walkFileTree(path, new SimpleFileVisitor<>() {
 
-    public static void showProjectFile(String path) {
-        File dir = new File(path);
-        File[] files = dir.listFiles();
-        if (files != null) {
-            for (File file: files) {
-                if (file.isFile()) {
-                    System.out.println("File: " + file.getName());
-                } else if (file.isDirectory()) {
-                    System.out.println("Directory: " + file.getName());
-                    showProjectFile(file.getAbsolutePath());
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                    int count = file.getNameCount() - path.getNameCount() + 1;
+                    count += file.getFileName().toString().length();
+
+                    String text = String.format("%" + count + "s", file.getFileName());
+                    text = text.replaceAll("[\\s]", "  ");
+                    System.out.println(text);
+                    return FileVisitResult.CONTINUE;
                 }
-            }
+
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+                    int count = dir.getNameCount() - path.getNameCount() + 1;
+                    count += dir.getFileName().toString().length();
+
+                    String text = String.format("%" + count + "s", dir.getFileName());
+                    text = text.replaceAll("[\\s]", "  ");
+                    System.out.println(text);
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            throw new StorageException("there was a problem reading the file ", path.getFileName().toString(), e);
         }
     }
 }

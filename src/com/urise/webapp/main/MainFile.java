50d0e4e4a35109da189350b4a532967cae5478f0
RASergey <ReshetkoSergey@mail.ru>
2020-05-05 13:53:47 +0300

    fix HW-9 MainFile

--- src/com/urise/webapp/main/MainFile.java
+++ src/com/urise/webapp/main/MainFile.java
@@ -6,16 +6,21 @@ public class MainFile {
 
     public static void main(String[] args) {
         File file = new File("./src");
-        showProjectFile(file);
+        showProjectFile(file, "");
     }
 
-    public static void showProjectFile(File file) {
-        if (file.isDirectory()) {
-            File[] files = file.listFiles();
-            assert files != null;
-            for (File value : files) {
-                System.out.printf("%" + (value.getName().length() + file.toPath().getNameCount()) + "s\n", value.getName());
-                showProjectFile(value);
+    public static void showProjectFile(File dir, String offset) {
+        if (dir.isDirectory()) {
+            File[] files = dir.listFiles();
+            if (files != null) {
+                for (File file: files) {
+                    if (file.isFile()) {
+                        System.out.println(offset + "F: " + file.getName());
+                    } else if (file.isDirectory()) {
+                        System.out.println(offset + "D: " + file.getName());
+                        showProjectFile(file, offset + "  ");
+                    }
+                }
             }
         }
     }
